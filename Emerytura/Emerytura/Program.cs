0106cd07ccbe32e8faf9d30730f9bdc8bd035f11
krwojtowicz <krwojtowiczz@gmail.com>
2022-10-16 19:36:12 +0200

    Emerytura exercises done

--- Emerytura/Emerytura/Program.cs
+++ Emerytura/Emerytura/Program.cs
@@ -4,7 +4,69 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!!");
+            string input = Console.ReadLine();
+            string[] splitedInput = input.Split(' ');
+            string nazwisko = splitedInput[0];
+            int wiekOsoby = Convert.ToInt32(splitedInput[1]);
+            int wiekEmerytalny = Convert.ToInt32(splitedInput[2]);
+
+            if(wiekOsoby < 0 || wiekEmerytalny < 0)
+            {
+                Console.WriteLine("Wiek nie może być ujemny!");
+            }
+            else if(wiekOsoby < wiekEmerytalny)
+            {
+                Console.Write($"Witaj {nazwisko}! ");
+                int lataDoEmerytury = wiekEmerytalny - wiekOsoby;
+                char[] liczbaJednosci = lataDoEmerytury.ToString().ToCharArray();
+       
+                    if(lataDoEmerytury == 1)
+                        Console.WriteLine($"Do emerytury brakuje Ci {lataDoEmerytury} rok!");
+                    else if(lataDoEmerytury < 5)
+                        Console.WriteLine($"Do emerytury brakuje Ci {lataDoEmerytury} lata!");
+                    else if (lataDoEmerytury < 22)
+                        Console.WriteLine($"Do emerytury brakuje Ci {lataDoEmerytury} lat!");
+                    else if (liczbaJednosci[1]=='2' || liczbaJednosci[1] == '3' || liczbaJednosci[1] == '4')
+                        Console.WriteLine($"Do emerytury brakuje Ci {lataDoEmerytury} lata!");
+                    else
+                        Console.WriteLine($"Do emerytury brakuje Ci {lataDoEmerytury} lat!");
+            }
+            else
+            {
+                Console.WriteLine($"Witaj emerycie {nazwisko}!");
+            }
+        }
+        static void Emerytura2()
+        {
+            string nazwisko = Console.ReadLine();
+            int wiekOsoby = Convert.ToInt32(Console.ReadLine());
+            int wiekEmerytalny = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Witaj, {nazwisko}!");
+            if (wiekOsoby < 0 || wiekEmerytalny < 0)
+            {
+                Console.WriteLine("Wiek nie może być ujemny!");
+            }
+            else if (wiekOsoby < wiekEmerytalny)
+            {
+                int lataDoEmerytury = wiekEmerytalny - wiekOsoby;
+                char[] liczbaJednosci = lataDoEmerytury.ToString().ToCharArray();
+
+                if (lataDoEmerytury == 1)
+                    Console.WriteLine($"Do emerytury brakuje Ci {lataDoEmerytury} rok!");
+                else if (lataDoEmerytury < 5)
+                    Console.WriteLine($"Do emerytury brakuje Ci {lataDoEmerytury} lata!");
+                else if (lataDoEmerytury < 22)
+                    Console.WriteLine($"Do emerytury brakuje Ci {lataDoEmerytury} lat!");
+                else if (liczbaJednosci[1] == '2' || liczbaJednosci[1] == '3' || liczbaJednosci[1] == '4')
+                    Console.WriteLine($"Do emerytury brakuje Ci {lataDoEmerytury} lata!");
+                else
+                    Console.WriteLine($"Do emerytury brakuje Ci {lataDoEmerytury} lat!");
+            }
+            else
+            {
+                Console.WriteLine("Jesteś emerytem!");
+            }
         }
     }
 }
