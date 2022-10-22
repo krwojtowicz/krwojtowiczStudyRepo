6754e68376bec5301b6252db7e5548463b5fa707
krwojtowicz <krwojtowiczz@gmail.com>
2022-10-22 23:56:34 +0200

    ParametryTrojkata exercise done!

--- ParametryTrojkata/ParametryTrojkata/Program.cs
+++ ParametryTrojkata/ParametryTrojkata/Program.cs
@@ -11,15 +11,33 @@ namespace Zadanie_ParametryTrojkata
             CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
             string[] userInput = Console.ReadLine().Trim().Split("; ");
             double[] inputNumbers = new double[3];
+            double shortestSide = double.MaxValue;
+            double longestSide = double.MinValue;
+            double midSide = 0;
             for(int i = 0; i < 3; i++)
             {
                 inputNumbers[i] = double.Parse(userInput[i]);
+                if (shortestSide >= inputNumbers[i])
+                {
+                    midSide = shortestSide;
+                    shortestSide = inputNumbers[i];
+                }
+                if(longestSide <= inputNumbers[i])
+                {
+                    midSide = longestSide;
+                    longestSide = inputNumbers[i];
+                }
+
                 if (inputNumbers[i] < 0)
                 {
                     Console.WriteLine("Błędne dane. Długości boków muszą być dodatnie!");
                     return;
                 }
+                
             }
+            Console.WriteLine(shortestSide);
+            Console.WriteLine(midSide);
+            Console.WriteLine(longestSide);
             double a = inputNumbers[0];
             double b = inputNumbers[1];
             double c = inputNumbers[2];
@@ -29,8 +47,25 @@ namespace Zadanie_ParametryTrojkata
                 Console.WriteLine("Błędne dane. Trójkąta nie można zbudować!");
                 return;
             }
-            // wczytaj dane
-            // i wykonaj obliczenia
+            else
+            {
+                double circuit = a + b + c;
+                double p = circuit / 2;
+                double field = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+                Console.WriteLine($"obwód = {Math.Round(circuit, 2).ToString("F2")}");
+                Console.WriteLine($"pole = {Math.Round(field, 2).ToString("F2")}");
+     
+                if ((shortestSide * shortestSide) + (midSide * midSide) == (longestSide * longestSide))
+                    Console.WriteLine("trójkąt jest prostokątny");
+                if ((shortestSide * shortestSide) + (midSide * midSide) > (longestSide * longestSide))
+                    Console.WriteLine("trójkąt jest ostrokątny");
+                if ((shortestSide * shortestSide) + (midSide * midSide) < (longestSide * longestSide))
+                    Console.WriteLine("trójkąt jest rozwartokątny");
+                if (a == b && b == c)
+                    Console.WriteLine("trójkąt równoboczny");
+                else if (a == b || a == c || b == c)
+                    Console.WriteLine("trójkąt równoramienny");
+            }
         }
     }
 }
